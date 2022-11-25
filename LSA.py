7cfd1ec5d26fb22a9bad34e3208af187c3c989ea
kshitiz <kshitiz.dhakal50@gmail.com>
2022-11-25 17:59:41 +0545

    Generalized LSA.py for any value of n_components

--- LSA.py
+++ LSA.py
@@ -1,3 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.decomposition import TruncatedSVD
+
+def lsa_calc(n):
+    svd = TruncatedSVD(n_components=n)
+    lsa = svd.fit_transform(bag_of_words)
+    return lsa
