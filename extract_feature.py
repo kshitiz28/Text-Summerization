c757c75b490452f76af5c526a2c36b26f9cd55b9
kshitiz <kshitiz.dhakal50@gmail.com>
2023-03-11 18:46:58 +0545

    Min length removed

--- extract_feature.py
+++ extract_feature.py
@@ -12,8 +12,7 @@ summarizer = pipeline('summarization')
 
 def abs_summary(article):
     l = len(list(article.split()))
-    abs_summ = summarizer(article, max_length=l, min_length=int(
-        l/2), do_sample=False)[0]['summary_text']
+    abs_summ = summarizer(article, max_length=l,do_sample=False)[0]['summary_text']
     return abs_summ
 
 
